4c3c0bb5efcf1260efcf45f761b958d17b56623f
GuiSmith <guilhermessmith2014@gmail.com>
2025-03-11 22:06:55 -0300

    banco e tabelas criadas

--- livro.sql
+++ livro.sql
@@ -1,9 +1,9 @@
--- Categoria
+
 CREATE TABLE categoria(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(100) NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Autor
 CREATE TABLE autor(
@@ -13,14 +13,14 @@ CREATE TABLE autor(
     biografia TEXT,
     nacionalidade VARCHAR(10) NOT NULL,
     foto VARCHAR(100),
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Editora
 CREATE TABLE editora (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cnpj VARCHAR(15) NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Livro
 CREATE TABLE livro (
@@ -29,5 +29,34 @@ CREATE TABLE livro (
     foto VARCHAR(100),
     sinopse TEXT,
     enredo TEXT,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- Estado
+CREATE TABLE biblioteca.estado_livro (
+    id SERIAL PRIMARY KEY,                -- Identificador único para cada estado
+    ativo BOOLEAN DEFAULT TRUE,            -- Se o estado está ativo ou não
+    descricao VARCHAR(255) NOT NULL,       -- Descrição do estado (ex: 'disponível', 'emprestado', 'danificado', etc.)
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Data de cadastro do estado
+);
+-- Entrada
+CREATE TABLE biblioteca.entrada (
+    id SERIAL PRIMARY KEY,
+    status CHAR CHECK (status IN ('A','F','C')) DEFAULT 'A',
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_fim TIMESTAMP DEFAULT NULL
+);
+-- Exemplar
+CREATE TABLE exemplar (
+    id SERIAL PRIMARY KEY,
+    id_livro INT NOT NULL,  -- Relaciona o exemplar com o livro correspondente
+    id_estado_livro INT NOT NULL,  -- Relaciona o exemplar com o estado correspondente
+    id_entrada INT NOT NULL,
+    edicao VARCHAR(40),
+    data_publicacao DATE,
+    data_aquisicao TIMESTAMP NOT NULL,  -- Data de aquisição do exemplar
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Data de cadastro do exemplar
+    status CHAR CHECK (status IN ('D', 'E', 'M', 'P', 'V', 'I')) DEFAULT 'D',  -- Status: disponível, emprestado, em manutenção, perdido, vendido, inutilizado
+    FOREIGN KEY (id_livro) REFERENCES livro(id),  -- Relaciona o exemplar com o livro
+    FOREIGN KEY (id_estado_livro) REFERENCES biblioteca.estado_livro(id),  -- Relaciona o exemplar com o estado do livro
+    FOREIGN KEY (id_entrada) REFERENCES biblioteca.entrada(id)
 );
--- usuario.sql
+++ usuario.sql
@@ -1,5 +1,5 @@
 -- Usuários 
-CREATE TABLE usuario (
+CREATE TABLE biblioteca.usuario (
     matricula BIGSERIAL,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(100),
@@ -7,43 +7,41 @@ CREATE TABLE usuario (
     data_nascimento DATE,
     senha VARCHAR(100),
     foto VARCHAR(100),
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_usuario PRIMARY KEY (matricula)
 );
 -- Tokens
-CREATE TABLE token (
+CREATE TABLE biblioteca.token (
     id SERIAL PRIMARY KEY,
     token VARCHAR(255) NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    data_expiracao DATETIME NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_expiracao TIMESTAMP NOT NULL,
     matricula_usuario BIGINT,
-    FOREIGN KEY (matricula_usuario) REFERENCES usuario(matricula)
+    FOREIGN KEY (matricula_usuario) REFERENCES biblioteca.usuario(matricula)
 );
 -- Telefones
-CREATE TABLE telefone (
+CREATE TABLE biblioteca.telefone (
     id SERIAL PRIMARY KEY,
     telefone VARCHAR(100),
     matricula_usuario BIGINT,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (matricula_usuario) REFERENCES usuario(matricula)
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (matricula_usuario) REFERENCES biblioteca.usuario(matricula)
 );
 -- Estados (UFs)
-CREATE TABLE uf(
+CREATE TABLE biblioteca.uf(
     id SERIAL PRIMARY KEY,
-    id_ibge BIGINT NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
     sigla VARCHAR(10) NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Cidades
-CREATE TABLE cidade(
+CREATE TABLE biblioteca.cidade(
     id SERIAL PRIMARY KEY,
-    id_ibge BIGINT NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Endereços
-CREATE TABLE endereco (
+CREATE TABLE biblioteca.endereco (
     id SERIAL PRIMARY KEY,
     rua VARCHAR(100) NOT NULL,
     numero VARCHAR(100) NOT NULL,
@@ -51,9 +49,9 @@ CREATE TABLE endereco (
     id_cidade INT,
     matricula_usuario BIGINT,
     id_editora INT,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_uf) REFERENCES uf(id),
-    FOREIGN KEY (id_cidade) REFERENCES cidade(id),
-    FOREIGN KEY (matricula_usuario) REFERENCES usuario(matricula),
-    FOREIGN KEY (id_editora) REFERENCES editora(id)
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_uf) REFERENCES biblioteca.uf(id),
+    FOREIGN KEY (id_cidade) REFERENCES biblioteca.cidade(id),
+    FOREIGN KEY (matricula_usuario) REFERENCES biblioteca.usuario(matricula),
+    FOREIGN KEY (id_editora) REFERENCES biblioteca.editora(id)
 );
