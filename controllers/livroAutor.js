7f5bd2f004e88538fcbd96ba7f9fb8928f00e7df
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-07 01:36:20 -0300

    listar autores e livros criados e testados

--- controllers/livroAutor.js
+++ controllers/livroAutor.js
@@ -1,22 +1,69 @@
+import '../models/relacionamentos.js';
 import livroAutor from "../models/livro_autor.js";
-import livro from "../models/livro.js";
-import autor from "../models/autor.js";
+import Livro from "../models/livro.js";
+import Autor from "../models/autor.js";
 
-const tabela = "livro_autor";
+const listarLivros = async (id_autor) => {
+    const livros = await livroAutor.findAll({
+        where: { id_autor: id_autor },
+        include: [{
+            model: Livro,
+            as: "livro",
+        }],
+    });
+
+    const autor = await Autor.findByPk(id_autor);
+
+    return {
+        autor: autor.dataValues,
+        livros: livros.map((livro) => ({
+            ...livro.livro.dataValues,
+            livro_autor: {
+                id: livro.id,
+                id_autor: livro.id_autor,
+                id_livro: livro.id_livro
+            }
+        })),
+    }
+};
+
+const listarAutores = async (id_livro) => {
+    const autores = await livroAutor.findAll({
+        where: { id_livro: id_livro },
+        include: [{
+            model: Autor,
+            as: 'autor',
+        }],
+    });
+
+    const livro = await Livro.findByPk(id_livro);
+
+    return {
+        livro: livro.dataValues,
+        autores: autores.map((autor) => ({
+            ...autor.autor.dataValues,
+            livro_autor: {
+                id: autor.id,
+                id_autor: autor.id_autor,
+                id_livro: autor.id_livro,
+            }
+        }))
+    };
+}
 
 const selecionar = async (id) => {
-    const registro =  await livroAutor.findByPk(id);
+    const registro = await livroAutor.findByPk(id);
     return registro.dataValues;
 }
 
 const inserir = async (id_autor, id_livro) => {
-    const registro = await livroAutor.create({id_autor, id_livro});
+    const registro = await livroAutor.create({ id_autor, id_livro });
     return registro.dataValues;
 };
 
 const alterar = async (id, id_autor, id_livro) => {
     // Atualiza o livroAutor
-    await livroAutor.update({id, id_autor, id_livro}, { where: { id: id } })
+    await livroAutor.update({ id, id_autor, id_livro }, { where: { id: id } })
         .then(result => result)
         .catch(err => err);
 };
@@ -27,15 +74,4 @@ const excluir = async (id) => {
         .catch(err => err);
 };
 
-(async () => {
-    console.log("Testando listar Livros");
-    await listarAutores(1)
-        .then(result => console.log(result))
-        .catch(err => console.log(err));
-    // console.log("Testando listar Autores");
-    // await listarAutores(1)
-    //     .then(result => console.log(result))
-    //     .catch(err => console.log(err));
-})();
-
-export default { selecionar, inserir, alterar, excluir };
+export default { selecionar, inserir, alterar, excluir, listarAutores, listarLivros };
