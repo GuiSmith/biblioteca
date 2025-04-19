be0eeb0eff01e8505448666053fda7d5f3a7764f
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-19 16:51:17 -0300

    prod test

--- banco.js
+++ banco.js
@@ -1,32 +1,16 @@
-const isProduction = process.env.NODE_ENV === "production";
-
-const sequelize = isProduction
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      protocol: "postgres",
-      dialectOptions: {
-          ssl: {
-              require: true,
-              rejectUnauthorized: false
-          }
-      },
-      define: {
-          timestamps: false,
-          freezeTableName: true
-      }
-  })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-          dialect: "postgres",
-          host: process.env.DB_HOST,
-          define: {
-              timestamps: false,
-              freezeTableName: true
-          }
-      }
-  );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+})
 
 export default sequelize;
