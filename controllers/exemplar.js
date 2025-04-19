fe2fa9df4734db8098545c9b00f2b6f14a1d2625
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-19 19:16:22 -0300

    listar e inserir iniciados

--- controllers/exemplar.js
+++ controllers/exemplar.js
@@ -1,10 +1,30 @@
 import Exemplar from '../models/exemplar.js';
+import Livro from '../models/livro.js';
 import util from './util.js';
 
 const listar = async (req, res) => {
-    const dados = await Exemplar.findAll();
-    const status = dados.length > 0 ? 200 : 204;
-    res.status(status).json(dados);
+
+    const livros = await Livro.findAll();
+
+    if(livros.length == 0){
+        return res.status(204).json({
+            mensagem: 'Nenhum livro cadastrado!'
+        });
+    }
+
+    const exemplares = await Exemplar.findAll({
+        where: {
+            id_livro: livros.map(livro => livro.id)
+        }
+    });
+
+    console.log(exemplares);
+
+    livros.exemplares = [...exemplares];
+
+    console.log(livros);
+
+    res.status(200).json(livros);
 }
 
 const selecionar = async (req, res) => {
@@ -26,12 +46,13 @@ const selecionar = async (req, res) => {
 }
 
 const inserir = async (req, res) => {
+    // Filtrando dados
     const requiredColumns = await util.requiredColumns(Exemplar.getTableName());
     const permittedColumns = await util.permittedColumns(Exemplar.getTableName());
 
     const data = util.filterObjectKeys(req.body, permittedColumns);
 
-    if(util.keysMatch(data, requiredColumns) == false){
+    if(!util.keysMatch(data, requiredColumns)){
         res.status(400).json({
             mensagem: 'Dados obrigatórios não informados',
             obrigatorios: requiredColumns,
@@ -41,7 +62,7 @@ const inserir = async (req, res) => {
 
     console.log(data);
 
-    await Exemplar.create(req.body)
+    await Exemplar.create(data)
         .then(result => res.status(201).json(result))
         .catch(err => res.status(500).json(err));
 }
