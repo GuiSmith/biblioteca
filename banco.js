57c124d029324491e4632ad7a56f3071fbcac87e
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-19 16:54:31 -0300

    imports added

--- banco.js
+++ banco.js
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
+
+import { Sequelize } from "sequelize";
+dotenv.config();
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     protocol: "postgres",
