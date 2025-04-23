16f715fd394f22f002e8b9bd1619782ce108146c
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-22 21:25:34 -0300

    retornando migrate para o natural

--- migrate.js
+++ migrate.js
@@ -1,6 +1,6 @@
 import sequelize from "./banco.js";
 // import './models/index.js';
-import Usuario from './models/usuario.js';
+import './models/index.js';
 
 await sequelize.sync({ alter: true });
 console.log("Tabelas criadas com sucesso");
