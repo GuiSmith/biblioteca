6eb46e4ba80afab389142a68470a5a5e653e7c95
GuiSmith <guilhermessmith2014@gmail.com>
2025-04-22 21:37:40 -0300

    cors changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 const corsOptions = {
-    origin: 'https://guismith.github.io/biblioteca',  // Domínio permitido
+    origin: 'https://guismith-library-6141e10fb627.herokuapp.com',  // Domínio permitido
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeçalhos permitidos
 };
