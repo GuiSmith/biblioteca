b9ea0210e2e4d26eff83c165adbb828ba148f6b6
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-02 22:09:15 -0300

    refatorados

--- controllers/autor.js
+++ controllers/autor.js
@@ -1,130 +1,182 @@
-import autor from '../models/autor.js';
+// Modelos
+import Autor from '../models/autor.js';
+import LivroAutor from '../models/livro_autor.js';
+// Controladores
 import util from './util.js';
 
-const tabela = 'autor';
-
 const listar = async (req, res) => {
-    await autor.findAll()
-        .then(result => res.status(result.length > 0 ? 200 : 204).json(result))
-        .catch(err => res.status(500).json({
-            mensagem: `Erro ao listar autores`,
-            erro: err
-        }));
+    try {
+        const autores = await Autor.findAll();
+        return res.status(autores.length > 0 ? 200 : 204).json(autores);
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
+        });
+    }
 }
 
 const selecionar = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({ mensagem: "ID não informado" });
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
+            return res.status(400).json({ mensagem: "ID não informado" });
+        }
+        const id = req.params.id;
+        // Verifica se o ID é um número
+        if (!util.isNumber(id)) {
+            return res.status(400).json({ mensagem: "ID inválido" });
+        }
+        // Seleciona o autor
+        const autorExistente = await Autor.findByPk(id);
+        if(!autorExistente){
+            return res.status(404).json({
+                mensagem: `Autor não encontrado`
+            });
+        }
+
+        return res.status(200).json(autorExistente);
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
+        });
     }
-    // Verifica se o ID é um número
-    if (!util.isNumber(req.params.id)) {
-        return res.status(400).json({ mensagem: "ID inválido" });
-    }
-    // Seleciona o autor
-    await autor.findByPk(req.params.id)
-        .then(result => res.status(result ? 200 : 204).json(result))
-        .catch(err => res.status(500).json({
-            mensagem: `Erro ao selecionar autor`,
-            erro: err
-        }));
 }
 
 const inserir = async (req, res) => {
-    // Filtrando dados
-    const requiredColumns = await util.requiredColumns(tabela);
-    const permittedColumns = await util.permittedColumns(tabela);
-    const data = util.filterObjectKeys(req.body, permittedColumns);
-    
-    // Verificando se os dados obrigatórios foram informados
-    if(util.keysMatch(data, requiredColumns) === false) {
-        return res.status(400).json({
-            mensagem: "Dados obrigatórios não informados",
-            obrigatorios: requiredColumns,
-            informados: Object.keys(data)
+    try {
+        // Filtrando dados
+        const requiredColumns = await util.requiredColumns(Autor.getTableName());
+        const permittedColumns = await util.permittedColumns(Autor.getTableName());
+        const data = util.filterObjectKeys(req.body, permittedColumns);
+
+        // Verificando se os dados obrigatórios foram informados
+        if (util.keysMatch(data, requiredColumns) === false) {
+            return res.status(400).json({
+                mensagem: "Dados obrigatórios não informados",
+                obrigatorios: requiredColumns,
+                informados: Object.keys(data)
+            });
+        }
+
+        //Verificando data de nascimento
+        if (data.hasOwnProperty('data_nascimento') && !util.validarData(data.data_nascimento)) {
+            return res.status(400).json({
+                mensagem: `Data inválida ${data.data_nascimento}`
+            });
+        }
+
+        // Criando o autor
+
+        const novoAutor = await Autor.create(data);
+
+        return res.status(201).json(novoAutor);
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
         });
     }
-
-    // Criando o autor
-    console.log(data);
-    await autor.create(data)
-        .then(result => res.status(201).json({
-            mensagem: `Autor ${result.nome} inserido com sucesso`,
-            autor: result
-        }))
-        .catch(err => res.status(500).json({
-            mensagem: `Erro ao inserir autor`,
-            erro: err
-        }));
 }
 
 const alterar = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({ mensagem: "ID não informado" });
-    }
-    const id = req.params.id;
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
+            return res.status(400).json({ mensagem: "ID não informado" });
+        }
+        const id = req.params.id;
 
-    // Verifica se o ID é um número
-    if (!util.isNumber(req.params.id)) {
-        return res.status(400).json({ mensagem: "ID inválido" });
-    }
-    // Verifica se o ID existe
-    const autorExistente = await autor.findByPk(req.params.id);
-    if (!autorExistente) {
-        return res.status(404).json({ mensagem: "Autor não encontrado" });
-    }
+        // Verifica se o ID é um número
+        if (!util.isNumber(id)) {
+            return res.status(400).json({ mensagem: "ID inválido" });
+        }
+        // Verifica se o ID existe
+        const autorExistente = await Autor.findByPk(id);
+        if (!autorExistente) {
+            return res.status(404).json({ mensagem: "Autor não encontrado" });
+        }
 
-    // Filtrando dados
-    const permittedColumns = await util.permittedColumns(tabela);
-    const data = util.filterObjectKeys(req.body, permittedColumns);
-    if(Object.keys(data).length == 0){
-        return res.status(400).json({
-            mensagem: "Nenhum dado informado para atualização",
-            permitidos: permittedColumns,
-            informados: Object.keys(data)
+        // Filtrando dados
+        const permittedColumns = await util.permittedColumns(Autor.getTableName());
+        const data = util.filterObjectKeys(req.body, permittedColumns);
+        if (Object.keys(data).length == 0) {
+            return res.status(400).json({
+                mensagem: "Nenhum dado informado para atualização",
+                permitidos: permittedColumns,
+                informados: Object.keys(data)
+            });
+        }
+
+        // Atualiza os dados
+        const [autorLinhasAlteradas] = await Autor.update(data, {
+            where: { id }
+        });
+
+        if (autorLinhasAlteradas > 0) {
+            return res.status(200).json({
+                mensagem: `Autor atualizado com sucesso`,
+            });
+        }
+
+        throw new Error("Autor não atualizado");
+
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
         });
     }
-
-    // Atualiza os dados
-    await autor.update(data, {
-        where: { id }
-    })
-        .then(result => res.status(200).json({
-            mensagem: `Autor atualizado com sucesso`,
-        }))
-        .catch(err => res.status(400).json({
-            mensagem: `Erro ao atualizar autor`,
-            erro: err
-        }));
 }
 
 const excluir = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({ mensagem: "ID não informado" });
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
+            return res.status(400).json({ mensagem: "ID não informado" });
+        }
+        const id = req.params.id;
+        // Verifica se o ID é um número
+        if (!util.isNumber(id)) {
+            return res.status(400).json({ mensagem: "ID inválido" });
+        }
+        // Verifica se o ID existe
+        const autorExistente = await Autor.findByPk(id);
+        if (!autorExistente) {
+            return res.status(404).json({ mensagem: "Autor não encontrado" });
+        }
+
+        // Verifica se há livros cadastrados com este autor
+        const livrosExistentes = await LivroAutor.findAll({
+            where: { id_autor: id }
+        });
+
+        if(livrosExistentes.length > 0){
+            return res.status(409).json({
+                mensagem: `Não é possível excluir autor, pois há livros vinculados`
+            });
+        }
+
+        // Exclui o autor
+        const autorLinhasExcluidas = await Autor.destroy({
+            where: { id }
+        });
+
+        if(autorLinhasExcluidas > 0){
+            return res.status(200).json({
+                mensagem: `Autor excluído com sucesso`
+            });
+        }
+        
+        throw new Error("Autor não excluído");
+        
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
+        });
     }
-    const id = req.params.id;
-    // Verifica se o ID é um número
-    if (!util.isNumber(id)) {
-        return res.status(400).json({ mensagem: "ID inválido" });
-    }
-    // Verifica se o ID existe
-    const autorExistente = await autor.findByPk(id);
-    if (!autorExistente) {
-        return res.status(404).json({ mensagem: "Autor não encontrado" });
-    }
-    // Exclui o autor
-    await autor.destroy({
-        where: { id }
-    })
-        .then(result => res.status(200).json({
-            mensagem: `Autor excluído com sucesso`
-        }))
-        .catch(err => res.status(500).json({
-            mensagem: `Erro ao excluir autor`,
-            erro: err
-        }));
 }
 
 export default { listar, selecionar, inserir, alterar, excluir };
--- controllers/usuario.js
+++ controllers/usuario.js
@@ -5,236 +5,267 @@ import Usuario from '../models/usuario.js';
 import util from './util.js';
 
 const listar = async (req, res) => {
-    const usuarios = await Usuario.findAll();
-    return res.status(usuarios.length > 0 ? 200 : 204).json(usuarios);
+    try {
+        const usuarios = await Usuario.findAll();
+        return res.status(usuarios.length > 0 ? 200 : 204).json(usuarios);
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
+        });
+    }
 };
 
 const selecionar = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({
-            mensagem: 'ID não informado'
-        });
-    }
-    const { id } = req.params;
-    // Verifica se o ID é um número
-    if (!util.isNumber(id)) {
-        return res.status(400).json({
-            mensagem: 'ID não é número'
-        });
-    }
-    const usuario = await Usuario.findByPk(id);
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
+            return res.status(400).json({
+                mensagem: 'ID não informado'
+            });
+        }
+        const { id } = req.params;
+        // Verifica se o ID é um número
+        if (!util.isNumber(id)) {
+            return res.status(400).json({
+                mensagem: 'ID não é número'
+            });
+        }
+        const usuario = await Usuario.findByPk(id);
 
-    if (!usuario) {
-        return res.status(404).json({
-            mensagem: `Usuário com ID ${id} não encontrado!`
+        if (!usuario) {
+            return res.status(404).json({
+                mensagem: `Usuário com ID ${id} não encontrado!`
+            });
+        }
+
+        return res.status(200).json(usuario);
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
         });
     }
-
-    return res.status(200).json(usuario);
 }
 
 const inserir = async (req, res) => {
-    // Filtrando dados
-    const requiredColumns = await util.requiredColumns(Usuario.getTableName());
-    const permittedColumns = await util.permittedColumns(Usuario.getTableName());
+    try {
+        // Filtrando dados
+        const requiredColumns = await util.requiredColumns(Usuario.getTableName());
+        const permittedColumns = await util.permittedColumns(Usuario.getTableName());
 
-    const data = util.filterObjectKeys(req.body, permittedColumns);
+        const data = util.filterObjectKeys(req.body, permittedColumns);
 
-    if (!util.keysMatch(data, requiredColumns)) {
-        res.status(400).json({
-            mensagem: 'Dados obrigatórios não informados',
-            obrigatorios: requiredColumns,
-            informados: Object.keys(data)
-        });
-    }
-
-    // Verifica se o CPF é válido
-    if (data.cpf) {
-        const novoCPF = util.normalizarCPF(data.cpf);
-        if (util.validarCPF(novoCPF)) {
-            data.cpf = novoCPF;
-        } else {
+        if (!util.keysMatch(data, requiredColumns)) {
             return res.status(400).json({
-                mensagem: 'CPF inválido',
-                cpf: data.cpf
+                mensagem: 'Dados obrigatórios não informados',
+                obrigatorios: requiredColumns,
+                informados: Object.keys(data)
             });
         }
-    }
 
-    if (data.senha) {
-        if (data.senha.length < 6 || data.senha.length > 20) {
-            return res.status(400).json({
-                mensagem: `A senha deve ter entre 6 e 20 caracteres`
-            });
-        }
-        data.senha = await util.criptografarSenha(data.senha);
-    }
-
-    // Verifica chaves únicas
-    const uniqueColumns = await util.uniqueColumns(Usuario.getTableName());
-
-    for (const column of uniqueColumns) {
-        if (data[column]) {
-            const exists = await util.checkUniqueColumn(Usuario, column, data[column]);
-            if (exists) {
-                return res.status(409).json({
-                    mensagem: `O valor para o dado '${column}' já está em uso`,
-                    dado: column,
-                    valor: data[column]
+        // Verifica se o CPF é válido
+        if (data.hasOwnProperty('cpf')) {
+            const novoCPF = util.normalizarCPF(data.cpf);
+            if (util.validarCPF(novoCPF)) {
+                data.cpf = novoCPF;
+            } else {
+                return res.status(400).json({
+                    mensagem: 'CPF inválido',
+                    cpf: data.cpf
                 });
             }
         }
-    }
 
+        if (data.hasOwnProperty('senha')) {
+            if (data.senha.length < 6 || data.senha.length > 20) {
+                return res.status(400).json({
+                    mensagem: `A senha deve ter entre 6 e 20 caracteres`
+                });
+            }
+            data.senha = await util.criptografarSenha(data.senha);
+        }
 
-    try {
-        const usuario = await Usuario.create(data);
-        return res.status(201).json(usuario);
+        // Verifica chaves únicas
+        const uniqueColumns = await util.uniqueColumns(Usuario.getTableName());
+
+        for (const column of uniqueColumns) {
+            if (data.hasOwnProperty(column)) {
+                const exists = await util.checkUniqueColumn(Usuario, column, data[column]);
+                if (exists) {
+                    return res.status(409).json({
+                        mensagem: `O valor para o dado '${column}' já está em uso`,
+                        dado: column,
+                        valor: data[column]
+                    });
+                }
+            }
+        }
+
+        try {
+            const usuario = await Usuario.create(data);
+            return res.status(201).json(usuario);
+        } catch (error) {
+            return res.status(500).json({
+                mensagem: 'Erro ao inserir usuário',
+                error
+            });
+        }
     } catch (error) {
         return res.status(500).json({
-            mensagem: 'Erro ao inserir usuário',
-            erro: error.message
+            mensagem: `Erro interno`,
+            error
         });
     }
 };
 
 const alterar = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({
-            mensagem: 'ID não informado'
-        });
-    }
-
-    const { id } = req.params;
-
-    // Verifica se o ID é um número
-    if (!util.isNumber(id)) {
-        return res.status(400).json({
-            mensagem: 'ID não é número'
-        });
-    }
-
-    // Verifica se o usuário existe
-
-    const usuario = await Usuario.findByPk(id);
-    if (!usuario) {
-        return res.status(404).json({
-            mensagem: `Usuário com ID ${id} não encontrado!`
-        });
-    }
-
-    // Filtrando dados
-    const permittedColumns = await util.permittedColumns(Usuario.getTableName());
-
-    const data = util.filterObjectKeys(req.body, permittedColumns);
-
-    if (data.cpf) {
-        const novoCPF = util.normalizarCPF(data.cpf);
-        if (util.validarCPF(novoCPF)) {
-            data.cpf = novoCPF;
-        } else {
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
             return res.status(400).json({
-                mensagem: 'CPF inválido',
-                cpf: data.cpf
+                mensagem: 'ID não informado'
             });
         }
-    }
 
-    // Verifica os valores unicos
-    const uniqueColumns = await util.uniqueColumns(Usuario.getTableName());
+        const { id } = req.params;
 
-    console.log(uniqueColumns);
+        // Verifica se o ID é um número
+        if (!util.isNumber(id)) {
+            return res.status(400).json({
+                mensagem: 'ID não é número'
+            });
+        }
 
-    for (const column of uniqueColumns) {
-        if (data[column]) {
-            const exists = await util.checkUniqueColumn(Usuario, column, data[column], id);
-            console.log(id, column, data[column], exists);
-            if (exists) {
+        // Verifica se o usuário existe
+
+        const usuario = await Usuario.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({
+                mensagem: `Usuário com ID ${id} não encontrado!`
+            });
+        }
+
+        // Filtrando dados
+        const permittedColumns = await util.permittedColumns(Usuario.getTableName());
+
+        const data = util.filterObjectKeys(req.body, permittedColumns);
+
+        if (data.hasOwnProperty('cpf')) {
+            const novoCPF = util.normalizarCPF(data.cpf);
+            if (util.validarCPF(novoCPF)) {
+                data.cpf = novoCPF;
+            } else {
                 return res.status(400).json({
-                    mensagem: `O valor para o dado '${column}' já está em uso`,
-                    dado: column,
-                    valor: data[column]
+                    mensagem: 'CPF inválido',
+                    cpf: data.cpf
                 });
             }
         }
-    }
 
-    if (data.senha) {
-        if (data.senha.length < 6 || data.senha.length > 20) {
-            return res.status(400).json({
-                mensagem: `A senha deve ter entre 6 e 20 caracteres`
-            });
+        // Verifica os valores unicos
+        const uniqueColumns = await util.uniqueColumns(Usuario.getTableName());
+
+        for (const column of uniqueColumns) {
+            if (data.hasOwnProperty(column)) {
+                const exists = await util.checkUniqueColumn(Usuario, column, data[column], id);
+                if (exists) {
+                    return res.status(400).json({
+                        mensagem: `O valor para o dado '${column}' já está em uso`,
+                        dado: column,
+                        valor: data[column]
+                    });
+                }
+            }
         }
-        data.senha = await util.criptografarSenha(data.senha);
-    }
 
-    try {
-        const usuario = await Usuario.update(data, {
+        if (data.hasOwnProperty('senha')) {
+            if (data.senha.length < 6 || data.senha.length > 20) {
+                return res.status(400).json({
+                    mensagem: `A senha deve ter entre 6 e 20 caracteres`
+                });
+            }
+            data.senha = await util.criptografarSenha(data.senha);
+        }
+
+        const [usuariosLinhasAtualizadas] = await Usuario.update(data, {
             where: { id }
         });
 
-        return res.status(200).json({
-            mensagem: `Usuário com ID ${id} alterado com sucesso!`,
-        });
+        if (usuariosLinhasAtualizadas > 0) {
+            return res.status(200).json({
+                mensagem: `Usuário com ID ${id} alterado com sucesso!`,
+            });
+        }
+
+        throw new Error("Usuário não atualizado");
+
     } catch (error) {
         return res.status(500).json({
-            mensagem: 'Erro ao alterar usuário',
-            erro: error.message
+            mensagem: `Erro interno`,
+            error
         });
     }
 };
 
 const definirSenha = async (req, res) => {
-    // Verifica se o ID foi informado
-    if (!req.params.id) {
-        return res.status(400).json({
-            mensagem: 'Informe o ID do usuário'
-        });
-    }
-    const id = req.params.id;
-
-    // Verifica se o usuário existe
-    const usuarioExistente = await Usuario.findByPk(id);
-    if (!usuarioExistente) {
-        return res.status(404).json({
-            mensagem: `Usuário com ID ${id} não encontrado`
-        });
-    }
-
-    // Filtrando dados
-    const dadosObrigatorios = ['senha'];
-    const data = util.filterObjectKeys(req.body, dadosObrigatorios);
-    if (!util.keysMatch(data, dadosObrigatorios)) {
-        return res.status(400).json({
-            mensagem: 'Dados obrigatórios não informados',
-            obrigatorios: dadosObrigatorios,
-            informados: Object.keys(data)
-        });
-    }
-
-    if (data.senha) {
-        if (data.senha.length < 6 || data.senha.length > 20) {
+    try {
+        // Verifica se o ID foi informado
+        if (!req.params.id) {
             return res.status(400).json({
-                mensagem: `A senha deve ter entre 6 e 20 caracteres`
+                mensagem: 'Informe o ID do usuário'
             });
         }
-        data.senha = await util.criptografarSenha(data.senha);
+        const id = req.params.id;
+
+        // Verifica se o usuário existe
+        const usuarioExistente = await Usuario.findByPk(id);
+        if (!usuarioExistente) {
+            return res.status(404).json({
+                mensagem: `Usuário com ID ${id} não encontrado`
+            });
+        }
+
+        // Filtrando dados
+        const dadosObrigatorios = ['senha'];
+        const data = util.filterObjectKeys(req.body, dadosObrigatorios);
+        if (!util.keysMatch(data, dadosObrigatorios)) {
+            return res.status(400).json({
+                mensagem: 'Dados obrigatórios não informados',
+                obrigatorios: dadosObrigatorios,
+                informados: Object.keys(data)
+            });
+        }
+
+        if (data.hasOwnProperty('senha')) {
+            if (data.senha.length < 6 || data.senha.length > 20) {
+                return res.status(400).json({
+                    mensagem: `A senha deve ter entre 6 e 20 caracteres`
+                });
+            }
+            data.senha = await util.criptografarSenha(data.senha);
+        }
+
+        data.token = null;
+
+        const [usuariosLinhasAtualizadas] = await Usuario.update(data, {
+            where: { id }
+        })
+
+        if(usuariosLinhasAtualizadas > 0){
+            return res.status(200).json({
+                mensagem: 'Senha definida com sucesso'
+            });
+        }
+
+        throw new Error("Senha não definida");
+        
+    } catch (error) {
+        return res.status(500).json({
+            mensagem: `Erro interno`,
+            error
+        })
     }
-
-    data.token = '';
-
-    return await Usuario.update(data, {
-        where: { id }
-    })
-        .then(result => res.status(200).json({
-            mensagem: 'Senha definida com sucesso'
-        }))
-        .catch(erro => res.status(500).json({
-            mensagem: 'Erro ao definir senha',
-            erro: erro
-        }))
 }
 
 export default { listar, selecionar, inserir, alterar, definirSenha };
